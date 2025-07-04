cde16f821c2e3fc9ed9c1c053150aa1535d404ed
Konstantin <kgranevitch@gmail.com>
2025-07-04 20:58:13 +0300

    add new functionl in class category

--- src/product.py
+++ src/product.py
@@ -7,7 +7,20 @@ class Product:
     quantity: int
 
     def __init__(self, name, description, price, quantity):
-        Product.name = name
-        Product.description = description
-        Product.price = price
-        Product.quantity = quantity
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, product_data):
+        """метод для создания объекта класса из словаря"""
+
+        return cls(product_data["name"], product_data["description"], product_data["price"], product_data["quantity"])
+
+
+if __name__ == "__main__":
+    prod1 = {"name": "banana", "price": 100.25, "description": "fruit", "quantity": 100}
+    obj1 = Product.new_product(prod1)
+    print(obj1.name)
+    print(obj1.price)
