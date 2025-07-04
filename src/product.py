207e9b6b9935e47e7d483818452f28bbedd1a82a
Konstantin <kgranevitch@gmail.com>
2025-07-04 21:10:29 +0300

    add new functional in class product

--- src/product.py
+++ src/product.py
@@ -9,7 +9,7 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
     @classmethod
@@ -18,9 +18,26 @@ class Product:
 
         return cls(product_data["name"], product_data["description"], product_data["price"], product_data["quantity"])
 
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price > 0:
+            self.__price = new_price
+        else:
+            print("Цена не должна быть нулевая или отрицательная")
+
 
 if __name__ == "__main__":
     prod1 = {"name": "banana", "price": 100.25, "description": "fruit", "quantity": 100}
     obj1 = Product.new_product(prod1)
     print(obj1.name)
+    print(obj1.price)
+
+    obj1.price = 51.55
+    print(obj1.price)
+
+    obj1.price = -10
     print(obj1.price)
